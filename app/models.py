76319dfb89e8b67afdea1dbf37fef31f122c2d5e
Vold73 <vold73@mail.ru>
2021-02-06 10:28:08 +0900

    add start_rent to auto database. fix database

--- app/models.py
+++ app/models.py
@@ -15,6 +15,7 @@ class Auto(db.Model):
     price = db.Column(db.Float())
     at = db.Column(db.Boolean)
     free = db.Column(db.Boolean)
+    start_rent = db.Column(db.DateTime)
     img_url1 = db.Column(db.String(128))
     img_url2 = db.Column(db.String(128))
     img_url3 = db.Column(db.String(128))
@@ -24,7 +25,7 @@ class Auto(db.Model):
 
 class RentTime(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    auto = db.Column(db.Integer)
+    auto = db.Column(db.Integer, db.ForeignKey('auto.id'))
     start_rent = db.Column(db.DateTime)
     end_rent = db.Column(db.DateTime)
     cost = db.Column(db.Float())
